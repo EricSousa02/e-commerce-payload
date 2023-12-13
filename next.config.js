b78c4b0fe71273ba73094727a04c349f6b3e9e98
EricSousa02 <ericsousadeandrade@gmail.com>
2023-12-13 16:53:16 -0300

    ignore

--- next.config.js
+++ next.config.js
@@ -3,6 +3,9 @@ const ContentSecurityPolicy = require('./csp')
 const redirects = require('./redirects')
 
 const nextConfig = {
+  typescrypt: {
+    ignoreBuildErrors: true
+  },
   reactStrictMode: true,
   swcMinify: true,
   images: {
